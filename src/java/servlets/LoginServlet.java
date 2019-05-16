24ac430caea129de0a923eccf38f2717fc110673
Pablo López Quiles <aiakos0@aol.com>
2019-05-16 10:14:57 -0500

    Fixed Login servlet

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import util.DBQueryHandler;
 
 /**
  *
@@ -30,17 +32,40 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        HttpSession session = request.getSession();
+        
+        String user="",pwd="";
+        
+        
+        if(request.getParameter("user")!=null && request.getParameter("pwd")!= null){
+            user = request.getParameter("user");
+            pwd = request.getParameter("pwd");
+        }else{
+            response.sendRedirect("index.html");
+        }
+                
+        System.out.println("Form data recovered: " + user + ":" + pwd );
+        
+        
+        if(DBQueryHandler.grantAccessToUser(user,pwd)){
+            session.setAttribute("valid-session", true);
+            session.setAttribute("username", user);
+            response.sendRedirect("home.jsp");
+        }else{
+            
+            try (PrintWriter out = response.getWriter()) {
+                /* TODO output your page here. You may use following sample code. */
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Servlet LoginServlet</title>");            
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1> Incorrect user or password</h1>");
+                out.println("<a href='index.html'> Return to login </a>");
+                out.println("</body>");
+                out.println("</html>");
+            }
         }
     }
 
